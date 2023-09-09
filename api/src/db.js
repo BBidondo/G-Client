6f8027beb7c886646122354dc093d3bf725f280f
Benjamin Bidondo <108904979+BBidondo@users.noreply.github.com>
2023-09-09 09:16:47 -0300

    .env changes railway

--- api/src/db.js
+++ api/src/db.js
@@ -2,15 +2,21 @@ require('dotenv').config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+  DB_DATABASE, // Utiliza la variable de entorno DB_DATABASE para el nombre de la base de datos
+  DB_USER,     // Utiliza la variable de entorno DB_USER para el usuario
+  DB_PASSWORD, // Utiliza la variable de entorno DB_PASSWORD para la contraseña
   {
+    host: DB_HOST,
+    port: DB_PORT, // Utiliza la variable de entorno DB_PORT para el puerto
+    dialect: 'postgres', // Usa el dialecto 'postgres' para PostgreSQL
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -25,9 +31,9 @@ fs.readdirSync(path.join(__dirname, "/models"))
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+// Injectamos la conexión (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+// Capitalizamos los nombres de los modelos, por ejemplo, product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
@@ -36,15 +42,15 @@ let capsEntries = entries.map((entry) => [
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
+// Para relacionarlos, hacemos un destructuring
 const { Videogame, Genre } = sequelize.models;
 
-// Aca vendrian las relaciones
+// Aquí vendrían las relaciones
 // Product.hasMany(Reviews);
 Videogame.belongsToMany(Genre, { through: "VideogameGenre" });
 Genre.belongsToMany(Videogame, { through: "VideogameGenre" });
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // Para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // Para importar la conexión { conn } = require('./db.js');
 };
